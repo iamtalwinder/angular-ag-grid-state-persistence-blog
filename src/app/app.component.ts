cd9d664d3fb0dc44c71db3707b25f3e8f70c5ade
Talwinder Singh <Talwinder.Singh@payever.org>
2022-08-27 23:10:20 +0530

    feat: add save state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ColDef, GridReadyEvent } from 'ag-grid-community';
+import { ColDef, ColumnApi, GridApi, GridReadyEvent } from 'ag-grid-community';
 import { Observable } from 'rxjs';
+import { 
+  AG_GRID_COLUMN_PERSISTENCE_KEY, 
+  AG_GRID_FILTER_PERSISTENCE_KEY, 
+  AG_GRID_GROUP_PERSISTENCE_KEY, 
+  AG_GRID_PAGINATION_PERSISTENCE_KEY, 
+  AG_GRID_PIVOT_PERSISTENCE_KEY,
+} from './constants';
+import { LocalStorageService } from './services';
 
 @Component({
  selector: 'app-root',
@@ -24,16 +32,55 @@ export class AppComponent {
  ];
 
  public defaultColDef: ColDef = {
-   sortable: true,
-   filter: true,
+  sortable: true,
+  filter: true,
+  resizable: true,
+  enableValue: true,
+  enablePivot: true,
+  enableRowGroup: true
  };
  
  public rowData$!: Observable<any[]>;
+ public gridApi!: GridApi;
+ public gridColumnApi!: ColumnApi;
 
- constructor(private http: HttpClient) {}
+ constructor(
+  private http: HttpClient,
+  private localStorageService: LocalStorageService,
+) {}
 
  onGridReady(params: GridReadyEvent) {
-   this.rowData$ = this.http
-     .get<any[]>('https://www.ag-grid.com/example-assets/olympic-winners.json');
+  this.gridApi = params.api;
+  this.gridColumnApi = params.columnApi;
+
+  this.rowData$ = this.http
+    .get<any[]>('https://www.ag-grid.com/example-assets/olympic-winners.json');
+ }
+
+
+ onFilterChanged() {
+  const filterState: any = this.gridApi.getFilterModel();
+
+  this.localStorageService.setItem(AG_GRID_FILTER_PERSISTENCE_KEY, filterState);
+ }
+
+ onSaveGridColumnState() {
+  const columnState: any = this.gridColumnApi.getColumnState();
+  const groupState: any = this.gridColumnApi.getColumnGroupState();
+
+  this.localStorageService.setItem(AG_GRID_COLUMN_PERSISTENCE_KEY, columnState);
+  this.localStorageService.setItem(AG_GRID_GROUP_PERSISTENCE_KEY, groupState);
+ }
+
+ onSavePivotModeState() {
+  const isPivotMode: boolean = this.gridColumnApi.isPivotMode();
+
+  this.localStorageService.setItem(AG_GRID_PIVOT_PERSISTENCE_KEY, isPivotMode);
+ }
+
+ onPaginationChanged() {
+  const pageNumber: Number = this.gridApi.paginationGetCurrentPage();
+
+  this.localStorageService.setItem(AG_GRID_PAGINATION_PERSISTENCE_KEY, pageNumber);
  }
 }
---
+++ src/app/constants.ts
@@ -0,0 +1,5 @@
+export const AG_GRID_FILTER_PERSISTENCE_KEY: string = 'ag-grid-filter-persistence-key';
+export const AG_GRID_COLUMN_PERSISTENCE_KEY: string = 'ag-grid-column-persistence-key';
+export const AG_GRID_GROUP_PERSISTENCE_KEY: string = 'ag-grid-group-persistence-key';
+export const AG_GRID_PAGINATION_PERSISTENCE_KEY: string = 'ag-pagination-filter-persistence-key';
+export const AG_GRID_PIVOT_PERSISTENCE_KEY: string = 'ag-grid-pivot-persistence-key';
