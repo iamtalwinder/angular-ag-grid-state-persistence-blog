ae66a09a9cebb34ba7e0017727753727f6007384
Talwinder Singh <Talwinder.Singh@payever.org>
2022-08-28 19:04:26 +0530

    feat: add restore state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ColDef, ColumnApi, GridApi, GridReadyEvent } from 'ag-grid-community';
-import { Observable } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 import { 
   AG_GRID_COLUMN_PERSISTENCE_KEY, 
   AG_GRID_FILTER_PERSISTENCE_KEY, 
@@ -44,10 +44,47 @@ export class AppComponent {
  public gridApi!: GridApi;
  public gridColumnApi!: ColumnApi;
 
+ /**Prevent writing into state, before the localstorage state has been restored */
+ private isGridStateRestored: boolean = false;
+
  constructor(
   private http: HttpClient,
   private localStorageService: LocalStorageService,
 ) {}
+
+onFirstDataRendered() {
+  const filterState: any = this.localStorageService.getItem(AG_GRID_FILTER_PERSISTENCE_KEY);
+
+  if (filterState) {
+    this.gridApi.setFilterModel(filterState);
+  }
+
+  const columnState: any = this.localStorageService.getItem(AG_GRID_COLUMN_PERSISTENCE_KEY);
+  const groupState: any = this.localStorageService.getItem(AG_GRID_GROUP_PERSISTENCE_KEY);
+
+  if (columnState) {
+    this.gridColumnApi.applyColumnState({
+      state: columnState,
+      applyOrder: true,
+    });
+  }
+
+  if (groupState) {
+    this.gridColumnApi.setColumnGroupState(groupState);
+  }
+
+  const isPivotMode: boolean = !!this.localStorageService.getItem(AG_GRID_PIVOT_PERSISTENCE_KEY);
+
+  this.gridApi.setPivotMode(isPivotMode);
+
+  const pageNumber: number = this.localStorageService.getItem(AG_GRID_PAGINATION_PERSISTENCE_KEY);
+
+  if (pageNumber !== null || pageNumber !== undefined) {
+    this.gridApi.paginationGoToPage(pageNumber);
+  }
+
+  this.isGridStateRestored = true;
+}
 
  onGridReady(params: GridReadyEvent) {
   this.gridApi = params.api;
@@ -59,12 +96,22 @@ export class AppComponent {
 
 
  onFilterChanged() {
+
+  if (!this.isGridStateRestored) {
+    return;
+  }
+
   const filterState: any = this.gridApi.getFilterModel();
 
   this.localStorageService.setItem(AG_GRID_FILTER_PERSISTENCE_KEY, filterState);
  }
 
  onSaveGridColumnState() {
+
+  if (!this.isGridStateRestored) {
+    return;
+  }
+
   const columnState: any = this.gridColumnApi.getColumnState();
   const groupState: any = this.gridColumnApi.getColumnGroupState();
 
@@ -73,12 +120,21 @@ export class AppComponent {
  }
 
  onSavePivotModeState() {
+
+  if (!this.isGridStateRestored) {
+    return;
+  }
+
   const isPivotMode: boolean = this.gridColumnApi.isPivotMode();
 
   this.localStorageService.setItem(AG_GRID_PIVOT_PERSISTENCE_KEY, isPivotMode);
  }
 
  onPaginationChanged() {
+  if (!this.isGridStateRestored) {
+    return;
+  }
+
   const pageNumber: Number = this.gridApi.paginationGetCurrentPage();
 
   this.localStorageService.setItem(AG_GRID_PAGINATION_PERSISTENCE_KEY, pageNumber);
